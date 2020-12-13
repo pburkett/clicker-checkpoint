f509aaa4abf63ea90b2292ae0d7182b9be3c7e2e
pburkett <pburkett636@gmail.com>
2020-12-12 23:02:26 -0700

    implemented upgrade unlock system + formatted

--- app.js
+++ app.js
@@ -4,21 +4,25 @@
 var count = 0
 var clickAmount = 1
 var passiveGenerationAmount = 0
-
+var incrementSpeed = 3000
 
 upgrades = {
-    dogFood: { name: "Dog Food", cost: 500, id: "dog-food-upgrade", img: "dog-food-image.png", description: "Reduce dog cost multiplier", upgradeLevel: 0, availability: "disabled" },
-    sheepDog: { name: "Sheep Dog", cost: 50, id: "sheep-dog-upgrade", img: "sheep-dog-image.png", description: "A friendly dog can count sheep themselves", upgradeLevel: 0, availability: "disabled" },
-    runningShoes: { name: "Running Shoes", cost: 30, id: "running-shoes-upgrade", img: "running-shoes.png", description: "Sheep are fast! Shoes will help you count more per click", upgradeLevel: 0, availability: "disabled" },
-    eyeGlasses: { name: "Counting Specs", cost: 500, id: "eye-glasses-upgrade", img: "glasses-image.png", description: "The better you see, the better you count!", upgradeLevel: 0, availability: "disabled" }
+    dogFood: { name: "Dog Food", cost: 250, id: "dog-food-upgrade", img: "dog-food-image.png", description: "Reduce dog cost multiplier", upgradeLevel: 0, availability: "disabled" },
+    sheepDog: { name: "Sheep Dog", cost: 50, id: "sheep-dog-upgrade", img: "sheep-dog-image.png", description: "A herding dog can count sheep themselves", upgradeLevel: 0, availability: "disabled" },
+    runningShoes: { name: "Running Shoes", cost: 30, id: "running-shoes-upgrade", img: "running-shoes.png", description: "Sheep are fast! Shoes will help you count more per click", upgradeLevel: 0, availability: "disabled" }
 }
+lockedUpgrades = {
+    eyeGlasses: { name: "Counting Specs", cost: 300, id: "eye-glasses-upgrade", img: "glasses-image.png", description: "The better you see, the better you count!", upgradeLevel: 0, availability: "disabled", unlocker: "runningShoes", unlockLevel: "10" },
+    sheedSpeep: { name: "Sheep Speed", cost: 500, id: "sheep-speed-upgrade", img: "sheep-speed-image.png", description: "If the sheep are faster, everything is faster", upgradeLevel: 0, availability: "disabled", unlocker: "sheepDog", unlockLevel: "15" }
+}
+
 var sheepDogCostMultiplier = 1
 
 
 
 
 function drawUpgradeCards() {
-
+    unlockUpgrades()
     let template = ''
     for (upgradeKey in upgrades) {
         if (upgrades[upgradeKey]['cost'] > count) {
@@ -29,10 +33,10 @@ function drawUpgradeCards() {
         template +=
             /*html*/
             `
-            <div class="col align-content-center upgrade-card">
+            <div class="col upgrade-card">
 
                 <div id="${upgrades[upgradeKey]['id']}" class="${(upgrades[upgradeKey]['availability'] ? 'cannot-afford ' : '')}row align-self-center justify-content-center upgrade-image" style="background-image: url(${upgrades[upgradeKey]['img']})">
-                    <div class="flex-column d-flex justify-content-end align-items-center">
+                    <div class="flex-column justify-self-center d-flex justify-content-end align-items-center">
                         
                         <button ${upgrades[upgradeKey]["availability"]} id="${upgrades[upgradeKey]['id']}-button" onclick="${upgradeKey}()" type="button" class="mb-1">Buy ${upgrades[upgradeKey]['name']}</button>
                         <h4>Cost:${upgrades[upgradeKey]['cost']}</h4>
@@ -49,7 +53,7 @@ function drawUpgradeCards() {
 
 
 function updateCount() {
-    document.getElementById("count-display").innerHTML = count
+    document.getElementById("count-display").innerHTML = count + ' Sheep'
     drawUpgradeCards()
 }
 
@@ -99,9 +103,34 @@ function eyeGlasses() {
 function incrementer() {
     count += passiveGenerationAmount
     updateCount()
+    if (passiveGenerationAmount > 0) {
+        document.getElementById("sheep-img").classList.add("sheep-img-scale")
+        setTimeout(() => {
+            document.getElementById("sheep-img").classList.remove("sheep-img-scale")
+        }, 400);
+    }
 }
+
+function unlockUpgrades() {
+
+    for (lockedUpgradeKey in lockedUpgrades) {
+        let lockedUpgrade = lockedUpgrades[lockedUpgradeKey]
+        let lockedUpgradeUnlocker = lockedUpgrade['unlocker']
+
+        if (upgrades[lockedUpgradeUnlocker]['upgradeLevel'] >= lockedUpgrade['unlockLevel']) {
+            upgrades[lockedUpgradeKey] = lockedUpgrade
+            delete lockedUpgrades[lockedUpgradeKey]
+
+
+        }
+
+    }
+}
+
 drawUpgradeCards()
-let increment = setInterval(() => incrementer(), 1000)
+
+let increment = setInterval(() => incrementer(), incrementSpeed)
+
 
 
 
