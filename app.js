41841e85f0da0e6faf276ccaf7930688a4b6a7af
pburkett <pburkett636@gmail.com>
2020-12-13 17:02:51 -0700

    changes

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var count = 0
 var clickAmount = 1
 var passiveGenerationAmount = 0
 var incrementSpeed = 3000
-
+var eyeGlassesIncreaseAmount = 50
 upgrades = {
-    dogFood: { name: "Dog Food", cost: 250, id: "dog-food-upgrade", img: "dog-food-image.png", description: "Reduce dog cost multiplier", upgradeLevel: 0, availability: "disabled" },
-    sheepDog: { name: "Sheep Dog", cost: 50, id: "sheep-dog-upgrade", img: "sheep-dog-image.png", description: "A herding dog can count sheep themselves", upgradeLevel: 0, availability: "disabled" },
-    runningShoes: { name: "Running Shoes", cost: 30, id: "running-shoes-upgrade", img: "running-shoes.png", description: "Sheep are fast! Shoes will help you count more per click", upgradeLevel: 0, availability: "disabled" }
+    dogFood: { name: "Dog Food", cost: 250, id: "dog-food-upgrade", img: "dog-food-image.png", description: "Help your dogs count faster", upgradeLevel: 0, maxLevel: 10, availability: "disabled" },
+    sheepDog: { name: "Sheep Dog", cost: 50, id: "sheep-dog-upgrade", img: "sheep-dog-image.png", description: "A herding dog can count sheep themselves", upgradeLevel: 0, maxLevel: 100, availability: "disabled" },
+    runningShoes: { name: "Running Shoes", cost: 30, id: "running-shoes-upgrade", img: "running-shoes.png", description: "Sheep are fast! Shoes will help you count more per click", upgradeLevel: 0, maxLevel: 12, availability: "disabled" }
 }
 lockedUpgrades = {
-    eyeGlasses: { name: "Counting Specs", cost: 300, id: "eye-glasses-upgrade", img: "glasses-image.png", description: "The better you see, the better you count!", upgradeLevel: 0, availability: "disabled", unlocker: "runningShoes", unlockLevel: "10" },
-    sheepSpeed: { name: "Sheep Speed", cost: 500, id: "sheep-speed-upgrade", img: "sheep-speed-image.png", description: "If the sheep are faster, everything is faster", upgradeLevel: 0, availability: "disabled", unlocker: "sheepDog", unlockLevel: "15" }
+    eyeGlasses: { name: "Counting Specs", cost: 1000, id: "eye-glasses-upgrade", img: "glasses-image.png", description: "The better you see, the better you count!", upgradeLevel: 0, maxLevel: 8, availability: "disabled", unlocker: "runningShoes", unlockLevel: "10" },
+    sheepSpeed: { name: "Sheep Speed", cost: 500, id: "sheep-speed-upgrade", img: "sheep-speed-image.png", description: "If the sheep are faster, everything is faster", upgradeLevel: 0, maxLevel: 3, availability: "disabled", unlocker: "dogFood", unlockLevel: "8" }
 }
 
 var sheepDogCostMultiplier = 1
@@ -25,11 +25,20 @@ function drawUpgradeCards() {
     unlockUpgrades()
     let template = ''
     for (upgradeKey in upgrades) {
+        let maxedCSSApplicator = ''
         if (upgrades[upgradeKey]['cost'] > count) {
             upgrades[upgradeKey]['availability'] = "disabled"
         } else {
             upgrades[upgradeKey]['availability'] = ""
         }
+        if (upgrades[upgradeKey]['upgradeLevel'] >= upgrades[upgradeKey]['maxLevel']) {
+            maxedCSSApplicator = 'maxed-upgrade'
+            upgrades[upgradeKey]['description'] = "Max Level"
+            upgrades[upgradeKey]['cost'] = ""
+            upgrades[upgradeKey]['name'] = ""
+            upgrades[upgradeKey]['availability'] = "disabled"
+        }
+
         template +=
             /*html*/
             `
@@ -37,10 +46,10 @@ function drawUpgradeCards() {
 
                 <div id="${upgrades[upgradeKey]['id']}" class="${(upgrades[upgradeKey]['availability'] ? 'cannot-afford ' : '')}row align-self-center justify-content-center upgrade-image  card-shadow" 
                     style="background-image: url(${upgrades[upgradeKey]['img']})">
-                    <div  class="col upgrade-description-container" id="upgrade-description-container-${upgrades[upgradeKey]['id']}">
+                    <div  class="col upgrade-description-container ${maxedCSSApplicator}" id="upgrade-description-container-${upgrades[upgradeKey]['id']}">
                         <h2 class="text-center upgrade-description-header default-cursor">${upgrades[upgradeKey]['name']}</h2>
                         <p class="text-center upgrade-description default-cursor">${upgrades[upgradeKey]['description']}</p>
-                        <h4 class="text-center">Cost:${upgrades[upgradeKey]['cost']}</h4>
+                        <h3 class="text-center text-shadow">${upgrades[upgradeKey]['cost']}</h3>
                     </div>
                     <div class="flex-column justify-self-center d-flex justify-content-end align-items-center default-cursor">
 
@@ -75,7 +84,7 @@ function sheepDog() {
     count -= upgrades['sheepDog']['cost']
     passiveGenerationAmount += 1
     upgrades['sheepDog']['cost'] += Math.floor(upgrades['sheepDog']['cost'] * sheepDogCostMultiplier)
-    drawUpgradeCards()
+    updateCount()
 }
 function runningShoes() {
     upgrades['runningShoes']['upgradeLevel'] += 1
@@ -83,37 +92,42 @@ function runningShoes() {
     updateCount()
     upgrades['runningShoes']['cost'] += Math.floor(upgrades['runningShoes']['cost'] * 1.2)
     clickAmount += 5
-    drawUpgradeCards()
+
 }
 
 function dogFood() {
+    upgrades['dogFood']['cost'] += Math.floor(upgrades['dogFood']['cost'] * .5)
+    upgrades['dogFood']['upgradeLevel'] += 1
+    count -= upgrades.dogFood.cost
+    updateCount()
 
-    if (sheepDogCostMultiplier >= .2) {
-        upgrades['dogFood']['upgradeLevel'] += 1
-        sheepDogCostMultiplier -= .05
-        count -= upgrades.dogFood.cost
-        drawUpgradeCards()
-    }
 }
 
 function eyeGlasses() {
     upgrades['eyeGlasses']['upgradeLevel'] += 1
     count -= upgrades['eyeGlasses']['cost']
-    drawUpgradeCards()
-    clickAmount += 25
+    clickAmount += eyeGlassesIncreaseAmount
+    eyeGlassesIncreaseAmount += eyeGlassesIncreaseAmount * 1
+    upgrades['eyeGlasses']['cost'] += Math.floor(upgrades['eyeGlasses']['cost'] * 2)
+    updateCount()
 }
 
 function sheepSpeed() {
-    if (incrementSpeed > 500) {
-        upgrades['sheepSpeed']['upgradeLevel'] += 1
-        count -= upgrades['sheepSpeed']['cost']
-        drawUpgradeCards()
-        incrementSpeed -= 500
-        let increment = setInterval(() => incrementer(), incrementSpeed)
-    }
+    upgrades['sheepSpeed']['upgradeLevel'] += 1
+    count -= upgrades['sheepSpeed']['cost']
+    updateCount()
+    incrementSpeed -= 500
+    let increment = setInterval(() => incrementer(), incrementSpeed)
+    upgrades['sheepSpeed']['cost'] += Math.floor(upgrades['sheepSpeed']['cost'] * 2)
 }
 
 function incrementer() {
+    let sheepDogMultiplier = 1
+
+    console.log(upgrades['dogFood']['upgradeLevel']);
+    sheepDogMultiplier += upgrades['dogFood']['upgradeLevel']
+
+    let passiveGenerationAmount = upgrades['sheepDog']['upgradeLevel'] * sheepDogMultiplier
     count += passiveGenerationAmount
     updateCount()
     if (passiveGenerationAmount > 0) {
